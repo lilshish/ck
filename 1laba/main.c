d4a1636753408c203731c7e0f53e15ea5e00c442
lilshish <vladyushashishkin95@gmail.com>
2021-12-11 20:28:51 +0300

    1laba

---
+++ 1laba/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    char str[10];
+    int int_t;
+    scanf("%s %d", &str, &int_t);
+    printf("%s %d", str, int_t);
+}
